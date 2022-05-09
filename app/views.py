38a3b0e10d9fd36485e0fb759ffa81a59f1412a6
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-09 22:15:01 +0200

    validated sign up form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
-from .forms import Signup
+from flask import redirect, render_template, url_for, redirect
+from .forms import LogIn, Signup
+
 
 @app.route('/')
 def index():
@@ -9,6 +10,16 @@ def index():
     '''
     return render_template('index.html')
 
-    
-    signup_form= Signup()
-    return render_template('signup.html', form=signup_form)
+@app.route("/login")
+def login():
+    login
+    return render_template('login.html', form=LogIn)
+
+@app.route("/sign-up", methods=['POST', 'GET'])
+def sign_up():
+    signup_form = Signup()
+    if signup_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template("signup.html", form=signup_form)  
+    # signup_form= Signup()
+    # return render_template('signup.html', form=signup_form)
