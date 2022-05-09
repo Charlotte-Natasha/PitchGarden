e7eb952e8a5ad183eec59d43f2fab5b3909c080a
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-09 17:45:53 +0200

    added signup class details

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
-import email
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,EmailField
-from wtforms.validators import DataRequired
+from wtforms import StringField,PasswordField,EmailField,SubmitField
+from wtforms.validators import DataRequired,Length,EqualTo,Email
 
 class Signup(FlaskForm):
-    username = StringField('Enter username', validators=[DataRequired()])
-    email = EmailField('Enter your email', validators=[DataRequired()])
-    password = PasswordField('Enter password', validators=[DataRequired()])
+    username = StringField('Enter username', validators=[DataRequired(),Length(min=3,max=20)])
+    email = EmailField('Enter your email', validators=[DataRequired(),Email()])
+    password = PasswordField('Enter password', validators=[DataRequired(),Length(min=5,max=15)])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField(label='Sign up')
